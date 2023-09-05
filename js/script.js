4087c3b69e19ab794a39d080e77c0322ecd0c63e
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2023-09-05 10:08:48 -0300

    Alterando arquivos

--- js/script.js
+++ js/script.js
@@ -1,63 +1,100 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-var espacoCano = 190;
-var posicaoCano = [];
-var velocidadeCano = 1;
-
-for (var i = 0; i < 9; i++) {
-    posicaoCano[i] = 500 + espacoCano * i;
+function areia(){
+    ctx.strokeStyle = '#fff6db';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.moveTo(0,700);
+    ctx.quadraticCurveTo(100,650,200,640);
+    ctx.quadraticCurveTo(300,630,450,700);
+    ctx.quadraticCurveTo(500,720,710,700);
+    ctx.quadraticCurveTo(780,690,1000,680);
+    ctx.quadraticCurveTo(1100,670,1700,680);
+    ctx.lineTo(1700,800);
+    ctx.lineTo(0,800);
+    ctx.lineTo(0,700);
+    ctx.fillStyle = '#f5e6bc';
+    ctx.fill()
+    ctx.stroke();
 }
 
-var movimentoPerso = 410;
-var veloPerso = 5;
-var veloAfundar = 0.5;
+function pedras(){
+    ctx.fillStyle = '#4f5561';
+    ctx.beginPath();
+    ctx.arc(150,750,4,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(300,720,5,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(400,710,5,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(200,690,3,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(1400,720,5,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(1100,700,3,0,Math.PI*2,true);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(1200,760,6,0,Math.PI*2,true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.moveTo(850,750);
+    ctx.quadraticCurveTo(860,710,910,725);
+    ctx.quadraticCurveTo(970,755,900,770);
+    ctx.quadraticCurveTo(850,775,850,750);
+    ctx.fillStyle = '#4f5561';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(850,750);
+    ctx.quadraticCurveTo(860,710,910,725);
+    ctx.quadraticCurveTo(910,735,850,750);
+    ctx.fillStyle = '#5b626e';
+    ctx.fill();
+}
 
 function obstaculos(){
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[0],0,10,330);
+    ctx.fillRect(posicaoCano[0],0,50,330);
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[0],470,10,330);
+    ctx.fillRect(posicaoCano[0],470,50,330);
     
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[1],0,10,150);
+    ctx.fillRect(posicaoCano[1],0,50,150);
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[1],300,10,500);
+    ctx.fillRect(posicaoCano[1],300,50,500);
 
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[2],0,10,450);
+    ctx.fillRect(posicaoCano[2],0,50,450);
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[2],600,10,200);
+    ctx.fillRect(posicaoCano[2],600,50,200);
 
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[3],0,10,350);
+    ctx.fillRect(posicaoCano[3],0,50,350);
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[3],500,10,300);
+    ctx.fillRect(posicaoCano[3],500,50,300);
 
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[4],0,10,450);
+    ctx.fillRect(posicaoCano[4],0,50,450);
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[4],600,10,200);
+    ctx.fillRect(posicaoCano[4],600,50,200);
 
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[5],0,10,150);
+    ctx.fillRect(posicaoCano[5],0,50,150);
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[5],300,10,500);
+    ctx.fillRect(posicaoCano[5],300,50,500);
 
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[6],0,10,250);
+    ctx.fillRect(posicaoCano[6],0,50,250);
     ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[6],400,10,400);
-
-    ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[7],0,10,550);
-    ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[7],700,10,100);
-
-    ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[8],0,10,450);
-    ctx.fillStyle = '#144f25'
-    ctx.fillRect(posicaoCano[8],600,10,200);
+    ctx.fillRect(posicaoCano[6],400,50,400);
 }
 
 function personagem(){
@@ -87,20 +124,39 @@ function tubarao(){
     ctx.fill();
 }
 
+var espacoCano = 250;
+var posicaoCano = [];
+var velocidadeCano = 1;
+
+for (var i = 0; i < 7; i++) {
+    posicaoCano[i] = 500 + espacoCano * i;
+}
+
+var movimentoPerso = 410;
+var veloPerso = 5;
+var gravidade = 0.5;
+
 function animar(){
     ctx.clearRect(0,0,1700,800);
     obstaculos();
     personagem();
     tubarao();
+    areia();
+    pedras();
 
-    for (var i = 0; i < 9; i++) {  
+    movimentoPerso += gravidade;
+    if(movimentoPerso >= 625){
+        movimentoPerso = 625;
+    }
+
+    for (var i = 0; i < 7; i++) {  
         posicaoCano[i] -= velocidadeCano;
 
-        if(posicaoCano[i] < 0){
+        if(posicaoCano[i] < -60){
             posicaoCano[i] = 1700;
         }
 
-        if(posicaoCano[8] < 0){
+        if(posicaoCano[6] < -60){
             velocidadeCano+=0.1;
         }
     }
@@ -108,7 +164,28 @@ function animar(){
     requestAnimationFrame(animar);
 }
 
+document.addEventListener('keydown', function(event){
+    personagem();
+    if(event.key === 'w'){
+        movimentoPerso -= veloPerso
+    }
+
+    else if(event.key === 's'){
+        movimentoPerso += veloPerso
+    }
+
+    if(movimentoPerso <= 0){
+        movimentoPerso = 0;
+    }
+
+    if(movimentoPerso >= 625){
+        movimentoPerso = 625;
+    }
+});
+
 animar();
 obstaculos();
 personagem();
 tubarao();
+areia();
+pedras();
